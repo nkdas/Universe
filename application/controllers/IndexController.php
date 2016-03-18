895ffe1901c31399ddc92351156473f1f5d2bcd9
Neeraj Kumar Das <nkdas91@gmail.com>
2016-03-18 18:54:30 +0530

    initialized signIn and signUp actions and created index action

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,16 +3,45 @@
 class IndexController extends Zend_Controller_Action
 {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
     public function indexAction()
     {
-        // action body
+
+        // Displays the SignIn form in index view
+        $signInForm = new Application_Form_SignIn();
+        $signInForm->submit->setLabel('Signin');
+        $this->view->signinForm = $signInForm;
+
+        // Displays the SignUp form in index view
+        $signUpForm = new Application_Form_SignUp();
+        $signUpForm->submit->setLabel('Signup');
+        $this->view->signupForm = $signUpForm;
+
+        if ($formData = $this->getRequest()->getPost()) {
+            if('Sign in' == ($this->getRequest()->getPost()['submit']) ) {
+                $this->signIn();
+            } else {
+                try{
+                    if($signUpForm->isValid($formData)){
+                        $this->signUp();
+                    }
+                    else{
+
+                    }
+                } catch (Exception $ex) {
+
+                }
+
+            }
+        }
     }
 
+    public function signIn()
+    {
+        echo 'Hello';
+    }
 
+    public function signUp()
+    {
+        echo 'Hi';
+    }
 }
-
